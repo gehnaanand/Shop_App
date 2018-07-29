0b977b97909010012633cf08eb61b152a6033cd8
GEHNA <gehnaanand@gmail.com>
2018-07-30 03:47:13 +0530

    tp

--- app/src/main/java/com/example/gehna/shopapp/SelectActivity.java
+++ app/src/main/java/com/example/gehna/shopapp/SelectActivity.java
@@ -3,11 +3,11 @@ package com.example.gehna.shopapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class HomeActivity extends AppCompatActivity {
+public class SelectActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_select);
     }
 }
